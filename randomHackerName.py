4f98837238a7a02e1088afd61117ef3c49997be9
THEw1tch <33990849+THEw1tch@users.noreply.github.com>
2020-01-17 17:38:43 -0800

    Update randomHackerName.py

--- randomHackerName.py
+++ randomHackerName.py
@@ -1,8 +1,20 @@
 
-#               made by the w1tch herself
 
 
-import random
+'''
+                            Made by the w!tch herself
+
+  Wanted to make all you cool cats take up the legend of the hackers listed here. 
+  Each one has their back story from movies, tv shows, games, and most important 
+  of all... real life. Yes, Most of these come from real life people who have 
+  made theirmark on the world. Will you take up the mantle of their hacker names 
+  and continue their legacy?
+
+'''
+
+import random as r
+import time as t
+import os, sys
 
 # Hacker names to choose from.
 hacker_Names = ['Neo','Trinity','Cipher',
@@ -19,9 +31,42 @@ hacker_Names = ['Neo','Trinity','Cipher',
                 'hunter','Acidus','geohot',
                 'Highrise Joe','hagbard','Lord Digital']
 
-# This gives you your hacker name.
-#print('Your hacker name is: ' + random.choice(hacker_Names) + '.')
+newName = r.choice(hacker_Names)
+key = "y"
+
+# Function that spits our your new name.
+def newday():
+    os.system('cls||clear')
+    t.sleep(1)
+    print("Hello, Friend")
+    t.sleep(2)
+    print("\nAre you ready to know your new name?: (y/n)\n")
+    answer = input()
+    if key == answer:
+        t.sleep(1)
+        print("\nOnce you know it... remember it.")
+        t.sleep(5)
+        os.system('cls||clear')
+        t.sleep(1)
+        print("\nWe won\'t remember for you")
+        t.sleep(3)
+        os.system('cls||clear')
+        t.sleep(1)
+        print("\nYour new name is now...\n\n")
+        t.sleep(3)
+        print("--------------------------------\n")
+        print("           " + newName + "\n")
+        print("--------------------------------\n\n")
+        t.sleep(13)
+        os.system('cls||clear')
+    else:
+        sys.exit(0)
+
+newday()
 
 # This print gives the full list.
-print(*hacker_Names, sep = "\n")
+#print(*hacker_Names, sep = "\n")
+
+
+
 
